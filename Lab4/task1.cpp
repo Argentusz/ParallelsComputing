fc310d11a16ea36b8d3182102753e1f06c404688
Argentusz <A4rgentum@yandex.ru>
2025-03-17 09:55:44 +0300

    Task #1. Rework

--- Lab4/task1.cpp
+++ Lab4/task1.cpp
@@ -18,11 +18,23 @@ int getSum(vector_t v) {
     return sum;
 }
 
+vector_t emptyVector(size_t size) {
+    vector_t vector = (vector_t)malloc(size * sizeof(element_t));
+    for (int i = 0; i < size; i++) vector[i] = 0;
+
+    return vector;
+}
+
 vector_t getRandomVector(size_t length, unsigned int seedOffset = 0) {
-    vector_t vector = (vector_t)malloc(length * sizeof(element_t));
+    vector_t vector = emptyVector(length);
     srand((unsigned int)time(NULL) + seedOffset);
     for (int i = 0; i < length; i++) vector[i] = rand() % 19 - 9;
     return vector;
+}
+
+void printVector(vector_t vector, int size) {
+    for (int i = 0; i < size; i++) printf("%d ", vector[i]);
+    printf("\n");
 }
 
 int main(int argc, char ** argv) {
@@ -37,20 +49,24 @@ int main(int argc, char ** argv) {
         return ERR_VAL;
     }
 
-    vector_t localVector = getRandomVector(SLICE_SIZE, rank);
-    vector_t sumVector = (vector_t)malloc(SLICE_SIZE * sizeof(element_t));
-    
-    printf("#%d Generated vector:", rank);
-    for (int i = 0; i < SLICE_SIZE; i++) printf(" %d", localVector[i]);
-    printf("\n");
+    size_t vectorSize = SLICE_SIZE * size;
+    vector_t vector = emptyVector(vectorSize);
+    if (rank == 0) {
+        vector = getRandomVector(vectorSize);
+        printf("Generated vector: ");
+        printVector(vector, vectorSize);
+    }
 
-    MPI_Reduce(localVector, sumVector, SLICE_SIZE, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+    vector_t slice = emptyVector(SLICE_SIZE);
+    MPI_Scatter(vector, SLICE_SIZE, MPI_ELEMENT_T, slice, SLICE_SIZE, MPI_ELEMENT_T, 0, MPI_COMM_WORLD);
+
+    vector_t sliceSum = emptyVector(SLICE_SIZE);
+    MPI_Reduce(slice, sliceSum, SLICE_SIZE, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
     
     if (rank == 0) {
         int sum = 0;
-        for (int i = 0; i < SLICE_SIZE; i++) sum += sumVector[i];
+        for (int i = 0; i < SLICE_SIZE; i++) sum += sliceSum[i];
         float average = (float)sum / (SLICE_SIZE * size);
-
         printf("Sum: %d Average: %f\n", sum, average);
     }
 
